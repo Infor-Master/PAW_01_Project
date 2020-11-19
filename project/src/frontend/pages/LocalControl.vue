f34c44a26ad6e418afe7a4ea740ba56dd08b4c49
Joao Reis <reisjoao97@hotmail.com>
2020-11-19 18:28:41 +0000

    nothing to declare

--- project/src/frontend/pages/LocalControl.vue
+++ project/src/frontend/pages/LocalControl.vue
@@ -1,17 +1,48 @@
 <template>
     <div>
-      <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
-      <p>Visit website</p>
-      <b-button variant="primary" href="#">More Info</b-button>
-  </b-jumbotron>
+      <b-container>
+        <b-row >
+          <b-col>
+            <b-jumbotron header="Local" lead="Person">
+              <h1>Number of people: </h1>
+              <b-button class="button" size=lg variant="success">Add Person</b-button>
+              <p></p>
+              <b-button class="button" size=lg variant="danger">Remove Person</b-button>
+              <VueTable />
+            </b-jumbotron>
+          </b-col>
+        </b-row>
+      </b-container>
+
+
+
+      <b-jumbotron header="Local" lead="Person">
+        <h1>Number of people: </h1>
+        <b-container class="bv-example-row">
+          <b-row>
+            <b-col>
+              <b-button class="button" size=lg variant="success">Add Person</b-button>
+           </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <b-button class="button" size=lg variant="danger">Remove Person</b-button>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-jumbotron>
 </div>
 </template>
 
 
+
 <script>
 
+import VueTable from '@/components/VueTable'
+
 export default {
-    
+  props: VueTable    
 }
 
 </script>
+
